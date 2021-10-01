4552b5521b0b9fc0fd89722f02e4a040b8f8dbe7
WalterCan <walterdaniel1316@gmail.com>
2021-10-01 17:28:50 -0300

    String o cadena de Texto

---
+++ js/string.js
@@ -0,0 +1,13 @@
+// String o cadenas de Texto
+
+// Forma de crear String
+
+const producto = 'Monitor de 20" Pulgadas';
+
+const producto2 = String('Monitor de 30" Pulgadas'); // A Ésto se lo conoce como constructor
+
+const producto3 = new String('Monitor de 50" Pulgadas'); //Crea un Objeto
+
+console.log(producto);
+console.log(producto2);
+console.log(typeof producto3); //Con TypeOf vamos a saber por consola que tipo es, si Object, String, etc.
