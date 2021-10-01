75d209fdce2f436e9f43fa43677dd14fab029016
WalterCan <walterdaniel1316@gmail.com>
2021-10-01 16:19:01 -0300

    Variables En JS

---
+++ js/01.js
@@ -0,0 +1,29 @@
+// Variables
+// Las Variables son Case Sensitive
+
+// Hay tres formas de escribir variables
+
+var producto = "Audifonos Gamers"; //Iniciar variable y asignarle un valor
+
+var disponible; //variable sin valor
+
+producto = true; // Reasignando el valor de la variable
+
+// Inicializar multiples variables para reutilizar el Var
+var producto1 = "Computadora",
+  disponible1 = true,
+  categoria = "computadoras";
+
+//   Reglas para crear variables
+// No pueden iniciar con numeros ni caracteres especiales tampoco con guin medio. Si con Guion Bajo!
+
+// var -disponible = true;
+// var 1categoria = "computadora";
+var _categoria = "Tablet";
+
+// Estilos para crear Variables
+
+var nombre_producto = "Monitor HD"; //Underscore
+var nombreProducto = "Monitor HD"; //Camelcase
+var NombreProducto = "Monitor HD"; //Pascalcase - se usa en Js solo cuando creamos Clases !
+var nombreproducto = "Monitor HD";
