3f4066ba1ce1e662cba77a7e79596181250e6b31
WalterCan <walterdaniel1316@gmail.com>
2021-10-01 17:02:06 -0300

    Variables Let y Const

---
+++ js/const.js
@@ -0,0 +1,10 @@
+// Variables Const
+// A diferencia de Let, Const no puede reasignarse, tampoco puede inicializar sin un valor
+// Lo utilizamos cuando son valores que no van a cambiar
+// si vamos a crear una variable y no sabemos el valor le asignamos Let
+
+const producto = "Monitor HD";
+
+const disponible = true;
+
+const numero = 12;
---
+++ js/let.js
@@ -0,0 +1,10 @@
+// Variables con let
+// Estas son similares a las variables Var. Siguen las mismas Reglas
+
+let producto = "Monitor HD";
+
+let disponible = true;
+
+let producto1 = "Monitor HD",
+  disponible1 = false,
+  categoria = "Computadoras";
