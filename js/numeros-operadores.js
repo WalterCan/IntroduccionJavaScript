9401b40652d77c96ebb4beb9d9de47a7e08aa26e
WalterCan <walterdaniel1316@gmail.com>
2021-10-06 15:38:55 -0300

    Numeros y Operaciones

---
+++ js/numeros-operadores.js
@@ -0,0 +1,20 @@
+// Numeros
+// Los Numeros en consola se ven de color azul, en cambio los string de color negro.
+// Éstas operaciones sirven para numeros, no para String
+
+const numero1 = 100;
+const numero2 = 200;
+const numero3 = 5;
+const numero4 = 15;
+
+console.log(numero1);
+console.log(numero2);
+console.log(numero3);
+console.log(numero4);
+
+// Operaciones
+
+console.log(numero1 + numero2);
+console.log(numero1 - numero2);
+console.log(numero4 / numero3);
+console.log(numero2 % numero1); //Resto de una división
