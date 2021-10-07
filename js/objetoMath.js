94bccddef894bb9b20490b5e2a7d5545f1db9907
WalterCan <walterdaniel1316@gmail.com>
2021-10-07 12:17:06 -0300

    Metodo Math

---
+++ js/objetoMath.js
@@ -0,0 +1,23 @@
+// Objeto Math
+
+resultado = Math.random(); //Numero Aleatorio
+resultado1 = Math.PI; //PI
+resultado2 = Math.round(3.7); //Redondea el número
+resultado3 = Math.ceil(5.8); //Redondea Hacia Arriba
+resultado4 = Math.floor(9.7); //Redondea Hacia Abajo
+resultado5 = Math.sqrt(144); //Raiz cuadrada de un Número
+resultado6 = Math.abs(-345); //convierte un numero negativo a positivo
+resultado7 = Math.min(-3, 4, 5, 6, 88, 9); //Muestra el número mas bajo de una lista
+resultado8 = Math.max(-3, 4, 5, 6, 88, 9); //Muestra El número más grande de una lista
+resultado9 = Math.floor(Math.random() * 30); //Utilizamos un numero random, pero éste simpre genera un numero menor a 1. Al usar el metodo Floor y multiplicando por otro Número vamos a generar numeros aleatorios mayores a 0
+
+console.log(resultado);
+console.log(resultado1);
+console.log(resultado2);
+console.log(resultado3);
+console.log(resultado4);
+console.log(resultado5);
+console.log(resultado6);
+console.log(resultado7);
+console.log(resultado8);
+console.log(resultado9);
