275f546e2695d6997c0fe559236f694739f3c113
WalterCan <walterdaniel1316@gmail.com>
2021-10-07 16:25:36 -0300

    Ordenes de Operaciones

---
+++ js/ordenesDeOperaciones.js
@@ -0,0 +1,27 @@
+// Orden de las Operaciones
+// si no hay parentesis tienen prioridad la multiplicacion y division
+let resultado;
+
+resultado = 20 + 15 * 16;
+resultado1 = (20 + 15) * 16;
+resultado2 = (15 + 2 + 4 + 6 + 8) * 0.5; //sacamos el porcentaje de la suma total
+resultado3 = (15 + 6 + 100 + 1) * 1.5; // al sumarle un 1 delante de la coma lo que hacemos es sacar el impuesto tota.La suma más el porsentaje
+
+console.log(resultado);
+console.log(resultado1);
+console.log(resultado2);
+console.log(resultado3);
+
+// Incrementos
+
+let incremento;
+
+incremento = 10;
+
+incremento++; //incremento de a uno
+
+incremento--; //Decremento
+
+incremento += 133; //Incremento o decremento una cierta cantidad
+
+console.log(incremento); // Primero incremento luego muestro.
