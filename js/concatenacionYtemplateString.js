d7b68eb354fe6c3a4933708631c866bc96aa4ebe
WalterCan <walterdaniel1316@gmail.com>
2021-10-12 12:28:58 -0300

    Concatenacion y Template String

---
+++ js/concatenacionYtemplateString.js
@@ -0,0 +1,13 @@
+const nombre = "Walter";
+const email = "Waltercantero1316@gmail.com";
+
+//Concatenacion -
+
+console.log("Nombre: " + nombre + " " + "Email: " + email);
+
+//Template String - String Relative (Nueva forma de hacerlo-Recomendada )
+
+//  `` -> Va todo dentro de estas dos comillas invertidas
+// ${} -> La variable va Dentro de éste Script, Tambien se pueden colocar Funciones
+
+console.log(`Nombre : ${nombre} Email: ${email}`);
