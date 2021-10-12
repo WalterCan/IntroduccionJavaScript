d852d4e051a9b9d971c346970c55f05e79febe16
WalterCan <walterdaniel1316@gmail.com>
2021-10-12 17:47:18 -0300

    Destructuring de Objetos

---
+++ js/destructuringDeObjetos.js
@@ -0,0 +1,13 @@
+const producto = {
+  nombreProducto: "Monitor 50 pulgadas",
+  precio: 600,
+  disponible: true,
+};
+
+// console.log(producto);
+// Extraemos Valores del objeto, creando una variable y asignandole el valor a la misma
+
+// Forma anterior
+
+const precioproducto = producto.precio;
+console.log(precioproducto);
