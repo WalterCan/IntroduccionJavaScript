df6d44805d033d7c23d4b59654710b8999a35e76
WalterCan <walterdaniel1316@gmail.com>
2021-10-14 16:05:45 -0300

    Destructuring Continuación

--- js/destructuringDeObjetos.js
+++ js/destructuringDeObjetos.js
@@ -9,5 +9,20 @@ const producto = {
 
 // Forma anterior
 
-const precioproducto = producto.precio;
-console.log(precioproducto);
+const precioProducto = producto.precio;
+const nombredelProducto = producto.nombreProducto;
+
+// console.log(precioProducto);
+// console.log(nombredelProducto);
+
+// Nueva forma de Extraer valores del objeto
+// Destructuring (sacar de una estructura)
+//  siempre que usamos destructuring : {propiedades} = objeto
+// Extrae el valor y crea la variable todo en un mismo paso
+// Para que Destructuring Funcionen deben el nombre de la variable debe ser igual a la propiedad del Objeto
+
+const { precio, nombreProducto } = producto; //agregamos todo en una misma líne.
+// const { nombreProducto } = producto;
+
+console.log(precio);
+console.log(nombreProducto);
