34f0db29b232df3c3416b44173503091d4979a83
WalterCan <walterdaniel1316@gmail.com>
2021-10-15 17:12:26 -0300

    Métodos para los Arrays- push, unshift, pop, splice

--- js/arrays.js
+++ js/arrays.js
@@ -30,3 +30,31 @@ meses.forEach(function (mes) {
 numeros.forEach(function (numero) {
   console.log(numero);
 });
+
+// Modificar elemento en un Arrays
+
+// Agregar elementos a un Arrays
+// metodo push -> agregar un elemento al final del array
+// Se recomienda no modificar los elementos originales de un Array
+
+// Estructura de datos -> Push, unshift
+
+numeros.push(60); //Se agrega al final
+numeros.unshift(-2); //Se agrega al principio del Arreglo.
+
+console.table(numeros);
+
+// Eliminar elementos de un Array
+
+meses.pop(); //Elimina el ultimo elemento del Array
+// meses.shift(); //Elimina el primer elemento de un Array
+console.table(meses);
+
+meses.splice(1, 1); //Primer valor la posicion.. segundo, cuantos elementos borrar luego de esa posición
+
+console.table(meses);
+
+// No se recomienda modificar los Arrays originales. PAra ello usamos :Rest OPerator o Sprad Operator
+
+const nuevoArreglo = [...meses, "Diciembre"];
+console.table(nuevoArreglo);
