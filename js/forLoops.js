7dae9eaa5d8b9fe00dd7c5e8e7865d9abe8b8f3c
WalterCan <walterdaniel1316@gmail.com>
2021-10-22 11:48:32 -0300

    Editando For Loops

--- js/forLoops.js
+++ js/forLoops.js
@@ -1,4 +1,5 @@
 // For Loops
+//Los For son la excelente forma de iterar sobre un array
 
 for (let i = 0; i <= 10; i++) {
   console.log(i);
@@ -6,8 +7,27 @@ for (let i = 0; i <= 10; i++) {
 
 // Evaluamos y mostramos los numeros pares
 
-for (let j = 0; j <= 100; j++) {
+for (let j = 0; j <= 10; j++) {
   if (j % 2 === 0) {
-    console.table(`Numero Par: ${j}`);
+    console.log(`Numero Par: ${j}`);
+  } else {
+    console.log(`Numero Impar: ${j}`);
   }
+}
+
+// Iterando sobre un Array
+
+const compra = [
+  { nombre: "Monitor de 22 Pulgadas", Precio: 400 },
+  { nombre: "Monitor de 19 Pulgadas", Precio: 300 },
+  { nombre: "Monitor de 28 Pulgadas", Precio: 550 },
+  { nombre: "Monitor de 32 Pulgadas", Precio: 700 },
+  { nombre: "Monitor de 40 Pulgadas", Precio: 900 },
+  { nombre: "Monitor de 52 Pulgadas", Precio: 1500 },
+  { nombre: "Monitor de 65 Pulgadas", Precio: 4000 },
+];
+
+for (let i = 0; i < compra.length; i++) {
+  console.log(compra[i]); //Iteramos Sobre todos los elementos
+  console.log(compra[i].nombre); //Iteramos solamente sobre el nombre
 }
