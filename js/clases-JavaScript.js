fb4a993bcd4cc89a0ee445959d59ef412eb16cb0
WalterCan <walterdaniel1316@gmail.com>
2021-10-28 17:48:50 -0300

    Inicio a Clases JavaScript

---
+++ js/clases-JavaScript.js
@@ -0,0 +1,24 @@
+// Clases en JavaScript
+// Nueva forma de Crear clases, ya no se usa Prototype
+// La Primer letra de una clase Va en Mayusculas
+
+// Sintaxis
+
+// class Producto {}
+
+class Producto {
+  constructo(nombre, precio) {
+    //Palabra Reservada "Constructor"
+    this.nombre = nombre;
+    this.precio = precio;
+  }
+  formatearProducto() {
+    return `El producto: ${this.nombre}, tiene un precio de $ ${this.precio}`;
+  }
+}
+
+// Instanciamos la clase
+
+const producto1 = new Producto("Monitor Curvo", 500);
+
+console.log(producto1);
